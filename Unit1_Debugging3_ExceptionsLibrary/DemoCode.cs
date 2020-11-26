3629e69c51fcb9c21b14a4c6f6b7a5859088a6ed
byfranfx <71705230+byfranfx@users.noreply.github.com>
2020-11-26 19:17:24 +0100

    Unit1_Debugging : 3 + Exception
    Video 3 completed!

---
+++ Unit1_Debugging3_ExceptionsLibrary/DemoCode.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unit1_Debugging3_ExceptionsLibrary
+{
+    public class DemoCode
+    {
+        public int GrandparentMethod(int position)
+        {
+            int output = 0;
+
+            Console.WriteLine("Open Database Connection");
+
+            try
+            {
+                output = ParentMethod(position);
+                if(position < 0)
+                {
+                    throw new IndexOutOfRangeException("The Value must be >= 0");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("You passed in bad data", ex);
+            }
+            finally
+            {
+                Console.WriteLine("Close Database Connection");
+            }
+            return output;
+        }
+
+        public int ParentMethod(int position)
+        {
+            return GetNumber(position);
+        }
+
+        public int GetNumber(int position)
+        {
+            int output = 0;
+            int[] numbers = new int[] { 1, 4, 7, 2 };
+            output = numbers[position];
+
+            return output;
+        }
+    }
+}
